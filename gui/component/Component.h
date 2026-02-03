0415f968b1507c78409afff705bf8efe771202c5
Lunix-420 <davidhess336@gmx.de>
2026-02-03 21:13:14 +0100

    Squashed 'external/dmt/' content from commit 43e0b73
    git-subtree-dir: external/dmt
    git-subtree-split: 43e0b73d60e146a0cbbcd87e636a37dbfc70b743

---
+++ gui/component/Component.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "./LinearSliderComponent.h"
+#include "./OscillatorDisplayComponent.h"
+#include "./RotarySliderComponent.h"
+#include "./SettingsEditorComponent.h"
---
+++ gui/widget/Widget.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "./Label.h"
+#include "./LinearSlider.h"
+#include "./RotarySlider.h"
+#include "./Shadow.h"
+#include "./TriangleButton.h"
