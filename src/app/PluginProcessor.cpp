93fbdcf2852e34e6ffc862b0bc568d175b797f7c
Lunix-420 <davidhess336@gmx.de>
2026-03-27 13:38:31 +0100

    refactor: Move code from PluginProcessor to AbstractPluginProcessor

--- src/app/PluginProcessor.cpp
+++ src/app/PluginProcessor.cpp
@@ -3,16 +3,7 @@
 #include "PluginEditor.h"
 //==============================================================================
 PluginProcessor::PluginProcessor()
-  : AudioProcessor(
-      BusesProperties()
-#if !JucePlugin_IsMidiEffect
-#if !JucePlugin_IsSynth
-        .withInput("Input", juce::AudioChannelSet::stereo(), true)
-#endif
-        .withOutput("Output", juce::AudioChannelSet::stereo(), true)
-#endif
-        )
-  , apvts(*this, nullptr, ProjectInfo::projectName, createParameterLayout())
+  : dmt::app::AbstractPluginProcessor(createParameterLayout)
   , oscilloscopeBuffer(2, 4096)
   , disfluxProcessor(apvts,
                      dmt::Settings::Audio::frequencySmoothness,
@@ -22,93 +13,16 @@ PluginProcessor::PluginProcessor()
                      dmt::Settings::Audio::outputHighpassFrequency,
                      dmt::Settings::Audio::smoothingInterval)
 {
-#if PERFETTO
-  MelatoninPerfetto::get().beginSession();
-#endif
   properties.initialize();
 }
 
-PluginProcessor::~PluginProcessor()
-{
-#if PERFETTO
-  MelatoninPerfetto::get().endSession();
-#endif
-}
+PluginProcessor::~PluginProcessor() = default;
 
 //==============================================================================
 const juce::String
 PluginProcessor::getName() const
 {
   return "Disflux";
-}
-
-bool
-PluginProcessor::acceptsMidi() const
-{
-#if JucePlugin_WantsMidiInput
-  return true;
-#else
-  return false;
-#endif
-}
-
-bool
-PluginProcessor::producesMidi() const
-{
-#if JucePlugin_ProducesMidiOutput
-  return true;
-#else
-  return false;
-#endif
-}
-
-bool
-PluginProcessor::isMidiEffect() const
-{
-#if JucePlugin_IsMidiEffect
-  return true;
-#else
-  return false;
-#endif
-}
-
-double
-PluginProcessor::getTailLengthSeconds() const
-{
-  return 0.0;
-}
-
-int
-PluginProcessor::getNumPrograms()
-{
-  return 1; // NB: some hosts don't cope very well if you tell them there are 0
-            // programs, so this should be at least 1, even if you're not really
-            // implementing programs.
-}
-
-int
-PluginProcessor::getCurrentProgram()
-{
-  return 0;
-}
-
-void
-PluginProcessor::setCurrentProgram(int index)
-{
-  juce::ignoreUnused(index);
-}
-
-const juce::String
-PluginProcessor::getProgramName(int index)
-{
-  juce::ignoreUnused(index);
-  return {};
-}
-
-void
-PluginProcessor::changeProgramName(int index, const juce::String& newName)
-{
-  juce::ignoreUnused(index, newName);
 }
 
 //==============================================================================
@@ -120,6 +34,7 @@ PluginProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
   disfluxProcessor.prepare(sampleRate);
 }
 
+//==============================================================================
 void
 PluginProcessor::releaseResources()
 {
@@ -127,51 +42,22 @@ PluginProcessor::releaseResources()
   // spare memory, etc.
 }
 
-bool
-PluginProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
-{
-#if JucePlugin_IsMidiEffect
-  juce::ignoreUnused(layouts);
-  return true;
-#else
-  // This is the place where you check if the layout is supported.
-  // In this template code we only support mono or stereo.
-  // Some plugin hosts, such as certain GarageBand versions, will only
-  // load plugins that support stereo bus layouts.
-  if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
-      layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-    return false;
-
-  // This checks if the input layout matches the output layout
-#if !JucePlugin_IsSynth
-  if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-    return false;
-#endif
-
-  return true;
-#endif
-}
-
+//==============================================================================
 void
 PluginProcessor::processBlock(juce::AudioBuffer<float>& buffer,
                               juce::MidiBuffer& midiMessages)
 {
+  // Boilerplate
   juce::ignoreUnused(midiMessages);
 
   juce::ScopedNoDenormals noDenormals;
   auto totalNumInputChannels = getTotalNumInputChannels();
   auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-  // In case we have more outputs than inputs, this code clears any output
-  // channels that didn't contain input data, (because these aren't
-  // guaranteed to be empty - they may contain garbage).
-  // This is here to avoid people getting screaming feedback
-  // when they first compile a plugin, but obviously you don't need to keep
-  // this code if your algorithm always overwrites all the output channels.
   for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
     buffer.clear(i, 0, buffer.getNumSamples());
 
-  //============================================================================
+  // Start actual processing
   TRACE_DSP();
   const auto* bypassParam = apvts.getRawParameterValue("GlobalBypass");
   bool isBypassed = bypassParam->load() > 0.5f;
@@ -183,39 +69,17 @@ PluginProcessor::processBlock(juce::AudioBuffer<float>& buffer,
 }
 
 //==============================================================================
-bool
-PluginProcessor::hasEditor() const
-{
-  return true; // (change this to false if you choose to not supply an editor)
-}
-
+// This creates new instances of the plugin..
 juce::AudioProcessorEditor*
 PluginProcessor::createEditor()
 {
   return new PluginEditor(*this);
 }
 
-//==============================================================================
-void
-PluginProcessor::getStateInformation(juce::MemoryBlock& destData)
-{
-  juce::MemoryOutputStream mos(destData, true);
-  apvts.state.writeToStream(mos);
-}
-
-void
-PluginProcessor::setStateInformation(const void* data, int sizeInBytes)
-{
-  auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
-  if (tree.isValid()) {
-    apvts.replaceState(tree);
-  }
-}
-
-//==============================================================================
-// This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE
 createPluginFilter()
 {
   return new PluginProcessor();
 }
+
+// KEEP
--- src/app/PluginProcessor.h
+++ src/app/PluginProcessor.h
@@ -5,7 +5,7 @@
 #include <JuceHeader.h>
 
 //==============================================================================
-class PluginProcessor final : public juce::AudioProcessor
+class PluginProcessor final : public dmt::app::AbstractPluginProcessor
 {
 public:
   //==============================================================================
@@ -13,39 +13,13 @@ public:
   ~PluginProcessor() override;
 
   //==============================================================================
+  const juce::String getName() const override;
   void prepareToPlay(double sampleRate, int samplesPerBlock) override;
   void releaseResources() override;
-
-  bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
-
   void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
-  using AudioProcessor::processBlock;
 
   //==============================================================================
   juce::AudioProcessorEditor* createEditor() override;
-  bool hasEditor() const override;
-
-  //==============================================================================
-  const juce::String getName() const override;
-
-  bool acceptsMidi() const override;
-  bool producesMidi() const override;
-  bool isMidiEffect() const override;
-  double getTailLengthSeconds() const override;
-
-  //==============================================================================
-  int getNumPrograms() override;
-  int getCurrentProgram() override;
-  void setCurrentProgram(int index) override;
-  const juce::String getProgramName(int index) override;
-  void changeProgramName(int index, const juce::String& newName) override;
-
-  //==============================================================================
-  void getStateInformation(juce::MemoryBlock& destData) override;
-  void setStateInformation(const void* data, int sizeInBytes) override;
-
-  //==============================================================================
-  juce::AudioProcessorValueTreeState apvts;
 
   //==============================================================================
   dmt::configuration::Properties properties;
@@ -62,9 +36,6 @@ public:
   void setSizeFactor(float newSize) { sizeFactor = newSize; }
 
 private:
-#if PERFETTO
-  std::unique_ptr<perfetto::TracingSession> tracingSession;
-#endif
   //==============================================================================
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PluginProcessor)
 };
