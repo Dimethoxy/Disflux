2255459df013b802a583934825a9cb0bc9d34138
Lunix-420 <davidhess336@gmx.de>
2026-04-09 18:38:23 +0200

    refactor: move even more logic to AbstractPluginEditor

--- src/app/PluginEditor.h
+++ src/app/PluginEditor.h
@@ -6,25 +6,12 @@
 //==============================================================================
 class PluginEditor : public dmt::app::AbstractPluginEditor
 {
-  using Image = juce::Image;
-  using ImageComponent = juce::ImageComponent;
-  using PixelFormat = juce::Image::PixelFormat;
-  using OpenGLContext = juce::OpenGLContext;
-
 public:
   explicit PluginEditor(PluginProcessor&);
   ~PluginEditor() override;
 
-  //==============================================================================
-  void paint(juce::Graphics&) override;
-  void resized() override;
-  void setConstraints(int width, int height);
-  void handleHeaderVisibilityChange(bool isHeaderVisible);
-
 private:
   //==============================================================================
-  PluginProcessor& p;
-  OpenGLContext openGLContext;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PluginEditor)
 };
